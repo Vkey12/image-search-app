02ce257f435023ce2abfd64e2866871f8bc0ba90
victor <victor.odhi@gmail.com>
2024-01-16 10:42:19 +0300

    called html element

---
+++ index.js
@@ -0,0 +1,12 @@
+const accessKey = "l76lDykETnr5MY2xW-vU11vGXAAx-MOxO_Wu3oD_-r0"
+
+// get the form element
+const formEl = document.querySelector("form")
+// get input element
+const inputEl = document.getElementById("search-input")
+//get search results element
+const searchResults = document.querySelector(".search-results")
+//get "show more" button element
+const showMore = document.getElementById("show-more-button")
+
+
